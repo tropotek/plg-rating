41b9831a646a8f868add31cb7fe7ab0934311a72
godar <info@tropotek.com>
2018-02-14 12:49:42 +1100

    ~Auto: Feature freeze now in place - Added Company Groups - fixed minor bugs

--- Rate/Ui/Stars.php
+++ Rate/Ui/Stars.php
@@ -19,10 +19,14 @@ class Stars
      */
     public static function create($stars = 0.0, $showValue = false)
     {
+        // TODO: Make this a config option in the plugin settings????
+        $dec = 0;
+
+        $stars = round($stars, $dec);
         $pct = round(($stars/5)*100);
         $value = '';
         if ($showValue)
-            $value = sprintf('<div class="value" var="ratingValue">%.2f</div>', (float)$stars);
+            $value = sprintf('<div class="value" var="ratingValue">%s</div>', $stars);
         return sprintf('<div class="star-rating"><div class="box"><div class="rating" style="width: %s%%;"></div></div>%s</div>', $pct, $value);
     }
 
