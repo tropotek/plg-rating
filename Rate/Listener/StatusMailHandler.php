8df74573156c545981bc5cd78c0691244e0e14cb
tropotek <info@tropotek.com>
2022-05-25 07:10:08 +1000

    ~Auto: Commit

--- Rate/Listener/StatusMailHandler.php
+++ Rate/Listener/StatusMailHandler.php
@@ -23,7 +23,7 @@ class StatusMailHandler implements Subscriber
         // do not send messages
         $course = \Uni\Util\Status::getCourse($event->getStatus());
         if (!$event->getStatus()->isNotify() || ($course && !$course->getCourseProfile()->isNotifications())) {
-            \Tk\Log::debug('Skill::onSendAllStatusMessages: Status Notification Disabled');
+            //\Tk\Log::debug('Skill::onSendAllStatusMessages: Status Notification Disabled');
             return;
         }
         $subject = \Uni\Util\Status::getSubject($event->getStatus());
